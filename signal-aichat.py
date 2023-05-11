a55a0d443ae4fe10acf847d919b677611570076e
cycneuramus <56681631+cycneuramus@users.noreply.github.com>
2023-05-11 16:58:47 +0200

    Fix trigger reset on every bot call, resulting in no response

--- signal-aichat.py
+++ signal-aichat.py
@@ -120,12 +120,16 @@ async def ai(ctx):
     disabled_models = os.getenv("DISABLED_MODELS", "").lower()
     default_model = os.getenv("DEFAULT_MODEL", "").lower()
 
-    triggers = {}
-    for model in MODELS:
-        if model not in ctx.data and model not in disabled_models:
-            ctx.data[model] = AI(model)
-        if model not in disabled_models:
-            triggers[ctx.data[model].trigger] = ctx.data[model].api
+    if "triggers" not in ctx.data:
+        triggers = {}
+        for model in MODELS:
+            if model not in ctx.data and model not in disabled_models:
+                ctx.data[model] = AI(model)
+            if model not in disabled_models:
+                triggers[ctx.data[model].trigger] = ctx.data[model].api
+        ctx.data["triggers"] = triggers
+    else:
+        triggers = ctx.data["triggers"]
 
     response = ""
     for trigger, api in triggers.items():
