9621938607b9225574cad10514b09f7e51b4c440
Paul Woitaschek <woitaschek@gmail.com>
2023-07-07 20:20:46 +0200

    Remove unnecessary comments

--- tests/test_signal_aichat.py
+++ tests/test_signal_aichat.py
@@ -5,17 +5,13 @@ import unittest
 
 class TestScriptImport(unittest.TestCase):
     def test_import_without_issues(self):
-        """Test if script executes without import errors"""
         try:
-            # Add the parent directory (where my_script.py resides) to the sys.path
             sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-            # Now Python should be able to import my_script
             import signal_aichat
             self.assertTrue(True)
 
         except ImportError as e:
-            # If there was an ImportError, make the test fail.
             self.fail(f"Import error: {e}")
 
 
